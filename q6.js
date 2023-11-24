b7c247231eafb6d6f116c249b2908773a08145ee
MiguelBoia <138613257+MiguelBoia@users.noreply.github.com>
2023-11-24 19:06:48 +0000

    Resolvi a questão.

---
+++ q6.js
@@ -0,0 +1,15 @@
+function quantosIguais (a, b, c) {
+    if (a==b && b==c){
+        return "Todos iguais!" 
+    }
+    if (a != b && b!=c && a!=c){
+        return "Todos diferentes."
+    }
+    else { return "Apenas um diferente."  }
+};
+
+const a = 1
+const b = 2
+const c = 1
+
+console.log(quantosIguais(a,b,c));
