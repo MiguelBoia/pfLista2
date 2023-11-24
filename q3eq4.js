072053ea5b67bff187dba2bacb5942803f4ebb62
MiguelBoia <138613257+MiguelBoia@users.noreply.github.com>
2023-11-24 16:10:50 +0000

    Conclui a questão 3 e 4.

--- q3eq4.js
+++ q3eq4.js
@@ -1,22 +1,22 @@
 function triangulo (lado1,lado2,lado3) {
     return (lado1 + lado2 > lado3 && lado2 + lado3 > lado1 && lado1 + lado3 > lado2 )
 } ;
-//console.log(triangulo(5,6,2));
+console.log(triangulo(2,2,2));
 
 function verificarTriangulo(lado1,lado2,lado3) {
-    if (lado1==lado2==lado3){
+    if (lado1==lado2 && lado2==lado3){
         return "Equilátero"
     }
-    if  (lado1 != lado2 && lado2 != lado3 && lado1 != lado3) {
-        return "Isóceles "
+    if (lado1 != lado2 && lado2 != lado3 && lado1 != lado3) {
+        return "Escaleno "
     }
-    else {return "Escaleno"}
+    else {return "Isóceles"}
 
 
 }
-const lado1 = 2
-const lado2 = 2
-const lado3 = 2
-if(triangulo(lado1,lado2, lado3)){
+const lado1 = 3
+const lado2 = 3
+const lado3 = 3
+if(triangulo(lado1,lado2,lado3)){
     console.log(verificarTriangulo(lado1,lado2,lado3))
 };
